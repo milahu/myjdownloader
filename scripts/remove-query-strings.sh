084df1f77ba2714bd4ba5b76b6c5f43534f7cb8e
Milan Hauth <milahu@gmail.com>
2024-09-13 22:11:32 +0200

    fix remove-query-strings.sh

--- scripts/remove-query-strings.sh
+++ scripts/remove-query-strings.sh
@@ -7,8 +7,14 @@ dirs=(
 for d in "${dirs[@]}"; do
   find "$d" -name '*\?*' | while IFS= read -r path; do
     path2=$(echo "$path" | sed 's/\?.*//')
-    echo "> mv ${path@Q} ${path2@Q}"
-    git mv "$path" "$path2"
-    git commit -m "mv $(basename "$path") $(basename "$path2")"
+    if ! [ -e "$path2" ]; then
+      echo "> mv ${path@Q} ${path2@Q}"
+      git mv "$path" "$path2"
+      git commit -m "mv $(basename "$path") $(basename "$path2")"
+    else
+      echo "> rm ${path@Q}"
+      git rm "$path"
+      git commit -m "rm $(basename "$path")"
+    fi
   done
 done

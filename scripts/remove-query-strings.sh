16a15b65ae9f6fc3a1a980567843117df26e7843
Milan Hauth <milahu@gmail.com>
2024-09-13 22:10:07 +0200

    add remove-query-strings.sh

---
+++ scripts/remove-query-strings.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dirs=(
+  my.jdownloader.org
+)
+
+for d in "${dirs[@]}"; do
+  find "$d" -name '*\?*' | while IFS= read -r path; do
+    path2=$(echo "$path" | sed 's/\?.*//')
+    echo "> mv ${path@Q} ${path2@Q}"
+    git mv "$path" "$path2"
+    git commit -m "mv $(basename "$path") $(basename "$path2")"
+  done
+done

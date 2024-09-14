3ec9e8a5ea7ea79b24c2b0eb461f18c85bd63b07
Milan Hauth <milahu@gmail.com>
2024-09-14 17:21:15 +0200

    fix fetch-urls.sh

--- scripts/fetch-urls.sh
+++ scripts/fetch-urls.sh
@@ -4,7 +4,7 @@ set -eux
 
 for url in "$@"; do
   # todo: also remove port
-  path=$(echo "$url" | sed 's|^https?://||')
+  path=$(echo "$url" | sed -E 's|^https?://||')
   mkdir -p "$(dirname "$path")"
   curl -L -o "$path" "$url"
 done
